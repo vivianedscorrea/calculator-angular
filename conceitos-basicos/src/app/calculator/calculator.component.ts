8409c488270d57db77b439c116daef80d3310835
FA98VXS <viviane.correa@fiserv.com>
2025-04-29 13:16:56 -0300

    Calculadora funcionando

--- conceitos-basicos/src/app/calculator/calculator.component.ts
+++ conceitos-basicos/src/app/calculator/calculator.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
-  imports: [],
+  imports: [ FormsModule ],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.scss'
 })
 export class CalculatorComponent {
 
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number = 0;
+
+  calcularResultado() {
+    console.log("Chamando o metodo para calcular o resultado!")
+    this.resultado = this.numero1 + this.numero2
+  }
 }

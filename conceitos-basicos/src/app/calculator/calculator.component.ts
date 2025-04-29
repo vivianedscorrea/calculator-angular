f1998bd2d7939c9b61f44cb433dfbb8a25d416bf
FA98VXS <viviane.correa@fiserv.com>
2025-04-29 14:55:52 -0300

    Renderização condicional com IF

--- conceitos-basicos/src/app/calculator/calculator.component.ts
+++ conceitos-basicos/src/app/calculator/calculator.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
-  imports: [ FormsModule ],
+  imports: [ FormsModule, CommonModule ],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.scss'
 })
